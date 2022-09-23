78aa863d1395648fee9da0cc7445e6af61f74cb4
warflash <nwiesinger67@gmail.com>
2022-09-23 20:54:51 +0200

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ m }}</div>
+</template>
+
+<script setup>
+const newLocale = 'en';
+const messages = await import(`./locales/${newLocale}.json`);
+const m = messages.default;
+</script>
